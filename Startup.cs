8592515dd09d45ecc2f485278f5523ac6406563d
Nave Kevin <kevnave82@gmail.com>
2018-10-13 00:22:12 +0200

    test branch comment

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,7 @@ namespace dotnetcore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            // test branch comment
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
